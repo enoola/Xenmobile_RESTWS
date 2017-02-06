9b94f7d2318e1d0c3827dfa086c4dd8e4fbb7ac1
enola <github@mobilutils.com>
2017-02-06 11:10:51 +0100

    added Server Properties

---
+++ src/class/Xenmobile_RESTWS_ServerProperties.php
@@ -0,0 +1,97 @@
+<?php
+namespace enoola_Citrix_Client;
+
+require_once('./class/Xenmobile_RESTWS_Exception.php');
+require_once('./class/Xenmobile_RESTWS_Authentication.php');
+
+class Xenmobile_RESTWS_ServerProperties extends Xenmobile_RESTWS_Authentication
+{
+  const SZ_WS_CLASSNAME = 'serverproperties';
+  private $_arImplementedMethod = array();
+
+  public function __construct($szFQDN, $nPort = parent::PORT_DEFAULT_HTTPS, $szProtocol = parent::PROTOCOL_HTTPS, $bVerifySSL = false)
+  {
+    $this->log('in', __METHOD__);
+    $this->log(self::SZ_WS_CLASSNAME, __METHOD__);
+    parent::__construct( $szFQDN, $nPort = parent::PORT_DEFAULT_HTTPS, $szProtocol = parent::PROTOCOL_HTTPS, $bVerifySSL = false);
+    parent::_setClassname( self::SZ_WS_CLASSNAME );
+  }
+
+  public function __destruct()
+  {
+    $this->log('in', __METHOD__);
+    parent::__destruct();
+  }
+
+  /*
+   * Get all current xenmobile server properties,
+   *
+   * @param array() arQuery ['start'=>0-999, 'limit'=>0-999, "sortOrder":"ASC,DESC,DSC","sortColumn":"ID,SERIAL,IMEI...","enableCount":"false]
+   * @return objStdClass() arQuery : {(int)status,(string)message,(objStdClass)currentFilter->(array)detail}
+   *
+   * cf : https://docs.citrix.com/en-us/xenmobile/10-3/xenmobile-rest-api-reference-main.html
+   */
+  public function GetAll(  )
+  {
+    $this->log('in', __METHOD__);
+
+    $retValue = $this->_doRequest(null, null, null, 'get');
+
+    if ( $retValue == true )
+    {
+      if ( $this->getLastRequestResult() && is_object($this->getLastRequestResult()) )
+        {
+          if ( isset( $this->getLastRequestResult()->status ) )
+            {
+              return ( $this->getLastRequestResult() );
+            }
+        }
+    }
+    elseif ($this->getLastHttpReturn()['http_code'] == 403)
+    {
+      throw new Xenmobile_RESTWS_Exception( $this->getLastRequestResult() );
+    }
+    if ($this->getLastRequestResult()->status != 0)
+    {
+        $this->log( $this->getLastRequestResult(), __METHOD__ );
+    }
+
+    throw new Xenmobile_RESTWS_Exception( $this->getLastRequestResult()->message, $this->getLastRequestResult()->status );
+  }
+
+
+  public function GetByFilter( $arQuery )
+  {
+    $this->log('in', __METHOD__);
+
+    $retValue = $this->_doRequest(null, null, $arQuery, 'post');
+
+    if ( $retValue == true )
+    {
+      if ( $this->getLastRequestResult() && is_object($this->getLastRequestResult()) )
+        {
+          if ( isset( $this->getLastRequestResult()->status ) )
+            {
+              return ( $this->getLastRequestResult() );
+            }
+        }
+    }
+    elseif ($this->getLastHttpReturn()['http_code'] == 403)
+    {
+      throw new Xenmobile_RESTWS_Exception( $this->getLastRequestResult() );
+    }
+    if ($this->getLastRequestResult()->status != 0)
+    {
+        $this->log( $this->getLastRequestResult(), __METHOD__ );
+    }
+
+    throw new Xenmobile_RESTWS_Exception( $this->getLastRequestResult()->message, $this->getLastRequestResult()->status );
+  }
+
+
+
+
+
+
+}
+?>
---
+++ src/example_ServerProperties.php
@@ -0,0 +1,51 @@
+<?Php
+namespace enoola_Citrix_Client;
+require_once('./class/Xenmobile_RESTWS_Exception.php');
+require_once('./class/Xenmobile_RESTWS_ServerProperties.php');
+
+/*
+*
+* Sample to use Xenmobile_RESTWS_Device
+*
+*/
+
+$szPathConfigFile = 'config_file.ini';
+if (!file_exists( $szPathConfigFile ) || is_link( $szPathConfigFile ) )
+{
+  die ('Config file not found : ' . $szPathConfigFile . PHP_EOL);
+}
+
+$arConfig = parse_ini_file( $szPathConfigFile );
+if ($arConfig === false)
+{
+  die ('Error reading config file :' . $szPathConfigFile . PHP_EOL);
+}
+
+if ( (!array_key_exists('fqdn', $arConfig) ) || (!array_key_exists('username', $arConfig) ) || (!array_key_exists('password', $arConfig) ) )
+  die ('Missing one or more mandatory field (fqdn, username, password).'.PHP_EOL);
+
+$oCliXM_WS = new Xenmobile_RESTWS_ServerProperties( $arConfig['fqdn'], 4443, 'https',false );
+
+if ( $oCliXM_WS->login($arConfig['username'], $arConfig['password'] ) == false )
+{
+  echo PHP_EOL.$oCliXM_WS->getLastRequestResult()->message.'json code:'.$oCliXM_WS->getLastRequestResult()->status.', ' .$oCliXM_WS->getLastHttpReturnCode().PHP_EOL;
+  exit (1);
+}
+echo 'result :' .PHP_EOL;
+
+print_r( $oCliXM_WS->GetAll() );
+
+
+
+
+//$arQuery = array('start'=>0, 'limit'=>9, "sortOrder"=>"ASC","sortColumn"=>"ID","enableCount"=>"false");
+//$arDevices = $oCliXM_WS->GetDeviceByFilters($arQuery);
+//print_r($arDevices);
+
+echo 'will logout' . PHP_EOL;
+if ( $oCliXM_WS->logout() == true)
+{
+  echo 'status : ' . $oCliXM_WS->getLastRequestResult()->Status . PHP_EOL;
+}
+
+?>
