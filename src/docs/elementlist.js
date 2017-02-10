77651bb630dc1186918dc970ec774294d3e2cbe6
enola <github@mobilutils.com>
2017-02-10 18:09:25 +0100

    removed generated docs

--- src/docs/elementlist.js
+++
@@ -1,3 +0,0 @@
-
-var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","enoola_Citrix_Client\\IXenmobile_RESTWS_Authentication"],["c","enoola_Citrix_Client\\Xenmobile_RESTWS_Abstract"],["c","enoola_Citrix_Client\\Xenmobile_RESTWS_Authentication"],["c","enoola_Citrix_Client\\Xenmobile_RESTWS_Device"],["c","enoola_Citrix_Client\\Xenmobile_RESTWS_Exception"],["c","enoola_Citrix_Client\\Xenmobile_RESTWS_LocalUsersGroups"],["c","enoola_Citrix_Client\\Xenmobile_RESTWS_ServerProperties"],["c","Exception"]];
